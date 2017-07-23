c2ca74ff24683df84afaf356d01d5d3a0e3a13a8
John Colbourne <j.colbourne@taurx.com>
2017-07-23 14:37:04 +0100

    settings.cs file with notificationID added for android

--- src/XamarinEvolve.Android/Helpers/Settings.cs
+++ src/XamarinEvolve.Android/Helpers/Settings.cs
@@ -1,44 +1,39 @@
-/*
-// Helpers/Settings.cs This file was automatically added when you installed the Settings Plugin. If you are not using a PCL then comment this file back in to use it.
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 
 namespace XamarinEvolve.Droid.Helpers
 {
-  /// <summary>
-  /// This is the Settings static class that can be used in your Core solution or in any
-  /// of your client applications. All settings are laid out the same exact way with getters
-  /// and setters. 
-  /// </summary>
-  public static class Settings
-  {
-    private static ISettings AppSettings
-    {
-      get
-      {
-        return CrossSettings.Current;
-      }
-    }
+	/// <summary>
+	/// This is the Settings static class that can be used in your Core solution or in any
+	/// of your client applications. All settings are laid out the same exact way with getters
+	/// and setters. 
+	/// </summary>
+	public static class Settings
+	{
+		static ISettings AppSettings
+		{
+			get
+			{
+				return CrossSettings.Current;
+			}
+		}
 
-    #region Setting Constants
+		const string NotificationIdKey = "notification_id";
+		static readonly int NotificationIdDefault = 0;
 
-    private const string SettingsKey = "settings_key";
-    private static readonly string SettingsDefault = string.Empty;
+		public static int NotificationId
+		{
+			get { return AppSettings.GetValueOrDefault<int>(NotificationIdKey, NotificationIdDefault); }
+			set
+			{
+				AppSettings.AddOrUpdateValue<int>(NotificationIdKey, value);
+			}
+		}
 
-    #endregion
+		public static int GetUniqueNotificationId()
+		{
+			return NotificationId++;
+		}
 
-
-    public static string GeneralSettings
-    {
-      get
-      {
-        return AppSettings.GetValueOrDefault<string>(SettingsKey, SettingsDefault);
-      }
-      set
-      {
-        AppSettings.AddOrUpdateValue<string>(SettingsKey, value);
-      }
-    }
-
-  }
-}*/
+	}
+}
